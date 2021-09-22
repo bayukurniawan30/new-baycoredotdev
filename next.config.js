a94223dca457c2640f2fa9766202feeaa80d975e
bayukurniawan30 <baycorephotography@gmail.com>
2021-09-22 16:36:47 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
-module.exports = {
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
+    env: {
+        NEXT_PUBLIC_WEB_URL: process.env.NEXT_PUBLIC_WEB_URL,
+        NEXT_PUBLIC_PURPLE_URL: process.env.NEXT_PUBLIC_PURPLE_URL,
+        PURPLE_KEY: process.env.PURPLE_KEY,
+        NEXT_PUBLIC_RECAPTCHA_SITEKEY: process.env.NEXT_PUBLIC_RECAPTCHA_SITEKEY,
+        EMAILJS_SERVICE_ID: process.env.EMAILJS_SERVICE_ID,
+        EMAILJS_TEMPLATE_ID: process.env.EMAILJS_TEMPLATE_ID,
+        EMAILJS_USER_ID: process.env.EMAILJS_USER_ID
+    },
     images: {
         domains: ['nextjs-bd.codewithbaycore.dev'],
+    },
+    pwa: {
+        dest: 'public'
     }
-}
+});
